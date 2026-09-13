663a17c2e1893c9205b3f815a66cf6a4c1254274
AbdelrahmanAbdallah <128175334+AbdelrahmanAbdallah789@users.noreply.github.com>
2026-09-13 13:04:28 +0300

    .

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use("/api/uploads", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
 app.use(cors({
-  origin: process.env.VITE_API_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }));
 
