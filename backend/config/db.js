96cd00bb630dc6952f23232d78b4b1fe51424eb9
AbdelrahmanAbdallah <128175334+AbdelrahmanAbdallah789@users.noreply.github.com>
2026-09-13 19:41:27 +0300

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     console.log(`Successfully connnected to mongoDB 👍`);
   } catch (error) {
     console.error(`ERROR: ${error.message}`);
+     process.exit(1);
   }
 };
 
