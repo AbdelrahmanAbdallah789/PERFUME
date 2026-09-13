e29126bb9cdd07feb736e05537375c89e0827ec9
AbdelrahmanAbdallah <128175334+AbdelrahmanAbdallah789@users.noreply.github.com>
2026-09-13 16:44:13 +0300

    .

--- api/index.cjs
+++ api/index.cjs
@@ -38,4 +38,4 @@ app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => console.log(`server running on port ${port}`));
 
-export default app
+module.exports = app; 
