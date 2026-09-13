163659c9e3baca638d10adabca262b61faa43a8a
AbdelrahmanAbdallah <128175334+AbdelrahmanAbdallah789@users.noreply.github.com>
2026-09-13 12:11:01 +0300

    .

--- api/index.js
+++ api/index.js
@@ -35,3 +35,5 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => console.log(`server running on port ${port}`));
+
+export default app
