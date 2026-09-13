d39d81cb9a91632d85e40203e0815a36e4810cf7
AbdelrahmanAbdallah <128175334+AbdelrahmanAbdallah789@users.noreply.github.com>
2026-09-13 12:27:35 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -35,5 +35,3 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => console.log(`server running on port ${port}`));
-
-export default app
