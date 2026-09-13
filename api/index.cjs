4fcf037b14af68cb7ae3fab6876c171e01eb4152
AbdelrahmanAbdallah <128175334+AbdelrahmanAbdallah789@users.noreply.github.com>
2026-09-13 16:50:13 +0300

    Update index.cjs

--- api/index.cjs
+++ api/index.cjs
@@ -36,9 +36,6 @@ app.get("/api/config/paypal", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
-if (process.env.NODE_ENV === 'true') {
-  const port = 5000;
-  app.listen(port, () => console.log(`Server running on port ${port}`));
-}
+app.listen(port, () => console.log(`server running on port ${port}`));
 
 module.exports = app; 
