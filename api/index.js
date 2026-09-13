7d355eed3a13dd6fa797710ae0cb13a148ba8e91
AbdelrahmanAbdallah <128175334+AbdelrahmanAbdallah789@users.noreply.github.com>
2026-09-13 16:16:09 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -36,6 +36,6 @@ app.get("/api/config/paypal", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
-// app.listen(port, () => console.log(`server running on port ${port}`));
+app.listen(port, () => console.log(`server running on port ${port}`));
 
 export default app
